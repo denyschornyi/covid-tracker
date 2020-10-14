9b632e2f72a6a4835bfe0e3198b5860d49a06852
Denys Chornyi <chornijj14@gmail.com>
2020-10-14 12:42:32 +0200

    Get data from APi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 
+const url ='https://disease.sh/v3/covid-19/countries' ;
+
 function App() {
   const [countries, setCountries] = useState(['USA', 'UK', 'Canada', 'Poland']);
+
+  useEffect(()=>{
+    const getCountries = async () => {
+      const res = await fetch(url);
+
+      if(!res.ok){
+        throw new Error(`Could not fetch ${url}, recived ${res.status}`)
+      }
+
+      return await res.json(); 
+    }
+    getCountries();
+  }, []);
 
   return (
     <div className="app">
