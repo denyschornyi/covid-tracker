120f73bb96eccfe5feb96ea8a7add3b027256282
Denys Chornyi <chornijj14@gmail.com>
2020-10-14 12:42:01 +0200

    Use useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 
 function App() {
+  const [countries, setCountries] = useState(['USA', 'UK', 'Canada', 'Poland']);
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>Covid-19 tracker</h1>
+        
         <FormControl className="app__dropdown">
           <Select
-            variant='outlined'
-            value='abc'
-          >
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
-            <MenuItem value='usa'>USA</MenuItem>
-            <MenuItem value="canada">Canada</MenuItem>
-            <MenuItem value="poland">Poland</MenuItem>
+              variant='outlined'
+              value='abc'
+            >
+            {countries.map(country => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
